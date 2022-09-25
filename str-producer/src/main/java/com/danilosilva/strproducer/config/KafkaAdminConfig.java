6336f9d7fe6ba15b0bb2f6b6f867d6ffb853b3ed
Danilo Silva <danilosilvap@outlook.com.br>
2022-09-25 17:16:48 -0300

    feat: add kafka in project with KafkaAdmin

---
+++ str-producer/src/main/java/com/danilosilva/strproducer/config/KafkaAdminConfig.java
@@ -0,0 +1,37 @@
+package com.danilosilva.strproducer.config;
+
+import static com.danilosilva.strproducer.support.Constant.STR_TOPIC_NAME;
+
+import java.util.HashMap;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaAdminConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        final var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+            TopicBuilder.name(STR_TOPIC_NAME)
+                .partitions(2)
+                .replicas(1)
+                .build()
+        );
+    }
+}
---
+++ str-producer/src/main/java/com/danilosilva/strproducer/support/Constant.java
@@ -0,0 +1,9 @@
+package com.danilosilva.strproducer.support;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constant {
+
+    public static final String STR_TOPIC_NAME = "str-topic";
+}
