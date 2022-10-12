c4f05766aec5d1f3596e0bd865b1c9e559700b8d
Danilo Silva <danilosilvap@outlook.com.br>
2022-10-12 16:34:07 -0300

    feat: determining partition for a specific consumer

--- str-consumer/src/main/java/com/danilosilva/strconsumer/listener/StrConsumerListener.java
+++ str-consumer/src/main/java/com/danilosilva/strconsumer/listener/StrConsumerListener.java
@@ -4,6 +4,7 @@ import static com.danilosilva.strconsumer.support.Constant.STR_TOPIC_NAME;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -13,6 +14,9 @@ public class StrConsumerListener {
     @KafkaListener(
         groupId = "group-1",
         topics = STR_TOPIC_NAME,
+        topicPartitions = {
+            @TopicPartition(topic = STR_TOPIC_NAME, partitions = {"0"})
+        },
         containerFactory = "strContainerFactory"
     )
     public void listener(final String message) {
@@ -22,6 +26,9 @@ public class StrConsumerListener {
     @KafkaListener(
         groupId = "group-1",
         topics = STR_TOPIC_NAME,
+        topicPartitions = {
+            @TopicPartition(topic = STR_TOPIC_NAME, partitions = {"1"})
+        },
         containerFactory = "strContainerFactory"
     )
     public void log(final String message) {
