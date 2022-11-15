7752ff4f3bcc401961e444731dec248b82ccece3
Danilo Silva <danilosilvap@outlook.com.br>
2022-11-15 18:44:58 -0300

    feat: more informations for handler

--- str-consumer/src/main/java/com/danilosilva/strconsumer/exception/KafkaListenerCustomHandler.java
+++ str-consumer/src/main/java/com/danilosilva/strconsumer/exception/KafkaListenerCustomHandler.java
@@ -1,5 +1,6 @@
 package com.danilosilva.strconsumer.exception;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.kafka.listener.ListenerExecutionFailedException;
@@ -11,8 +12,12 @@ import org.springframework.stereotype.Component;
 public class KafkaListenerCustomHandler implements KafkaListenerErrorHandler {
 
     @Override
-    public Object handleError(Message<?> message, ListenerExecutionFailedException e) {
+    public Object handleError(@NonNull Message<?> message, @NonNull ListenerExecutionFailedException exception) {
         log.error("Exception handler ::: Get error");
+        log.info("Payload ::: {}", message.getPayload());
+        log.info("Headers ::: {}", message.getHeaders());
+        log.info("Offset ::: {}", message.getHeaders().get("kafka_offset"));
+        log.info("Message exception ::: {}", exception.getMessage());
         return null;
     }
 }
